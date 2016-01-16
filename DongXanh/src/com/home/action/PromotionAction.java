f8392a52fe2f1c10d3bfeea3c773618a1c8e4f18
tttoan <thientran710@gmail.com>
2016-01-16 12:09:36 +0700

    TTToan

---
+++ DongXanh/src/com/home/action/PromotionAction.java
@@ -0,0 +1,17 @@
+package com.home.action;
+
+import com.opensymphony.xwork2.Action;
+/**
+ * 
+ * @author USER
+ *
+ */
+public class PromotionAction implements Action{
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+}
